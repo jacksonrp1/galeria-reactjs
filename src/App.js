ce92163a99234010ec7d10f22ba8f3bb5bd8165d
Jackson Rodrigues <jacksonrodriguesdepaula@gmail.com>
2022-11-17 22:49:06 -0300

    Import da Api pexels

--- src/App.js
+++ src/App.js
@@ -1,39 +1,47 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './style.css'
 import WinDimensions from './getWidth.js'
-import { url, token } from './api.js'
+import { token } from './api.js'
+import { createClient } from 'pexels'
 
 function App() {
   const [ArrayPhotos, setArrayPhotos] = useState([])
-  const { height, width } = WinDimensions()
+  const { width } = WinDimensions()
+  const target = '_blank'
 
-  window.addEventListener('load', () => {
-    async function buscaPhotos() {
-      const req = await fetch(`${url}/`, {
-        cache: 'default',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: token,
-          Accept: 'application/json'
-        },
-        method: 'GET'
-      }).then(res => res.json())
-      const res = await req.photos
-      setArrayPhotos(res)
-    }
-    buscaPhotos()
-  })
+  useEffect(() => {
+    const cliente = createClient(token)
+    const page = Math.floor(Math.random() * 200)
+
+    cliente.photos.curated({ page: page, per_page: 32 }).then(photos => {
+      setArrayPhotos(photos.photos)
+    })
+    console.log('Projeto Galeria - https://jacksondev.com.br/')
+  }, [])
+  const handleClick = index => {
+    const divPhoto = document.querySelectorAll('.galeria div')
+    divPhoto[index].classList.toggle('active')
+  }
 
   return (
     <div className="galeria">
       {ArrayPhotos.map((photo, index) => (
         <div
+          onClick={() => {
+            handleClick(index)
+          }}
           key={index}
           className={`${
             index % 2 === 1 ? (width <= 425 ? 'r2' : 'r1') : 'r2'
           } ${index % 2 === 0 ? (width <= 425 ? 'c2' : 'c1') : 'c2'}`}
         >
-          <img src={photo.src.large2x} alt="" />
+          <img src={photo.src.large2x} alt={photo.alt} />
+          <a href={photo.photographer_url} target={target}>
+            <p className="photographer">
+              Fotografo: <br></br>
+              {photo.photographer}
+            </p>
+          </a>
         </div>
       ))}
     </div>
