706c0df62cdc12293959cba74c063fff4ca4120e
Jackson Rodrigues <jacksonrodriguesdepaula@gmail.com>
2022-11-17 22:50:02 -0300

    Token

--- src/api.js
+++ src/api.js
@@ -1,5 +1,3 @@
-const page = Math.floor(Math.random() * (200 - 1) + 1)
-const url = `https://api.pexels.com/v1/curated?page=${page}&per_page=32`
 const token = 'Sua chave Token'
 
-module.exports = { url, token }
+export { token }
